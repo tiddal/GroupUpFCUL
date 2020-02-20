96a273e9e0e4dcf3e19d61fd1372fe6b1d0a2f92
tiddal <tiddal@sapo.pt>
2020-02-20 16:11:24 +0000

    delete unnecessary comments

--- API/routes/index.js
+++ API/routes/index.js
@@ -5,24 +5,4 @@ const usersRoutes = require('./users.js');
 app.use(express.json());
 app.use('/users', usersRoutes);
 
-// router.get('/users', async (req, res, next) => {
-// 	try {
-// 		let results = await db.users.all();
-// 		res.json(results);
-// 	} catch (err) {
-// 		console.log(err);
-// 		res.sendStatus(500);
-// 	}
-// });
-
-// router.get('/users/:id', async (req, res, next) => {
-// 	try {
-// 		let results = await db.users.byId(req.params.id);
-// 		res.json(results);
-// 	} catch (err) {
-// 		console.log(err);
-// 		res.sendStatus(500);
-// 	}
-// });
-
 module.exports = app;
