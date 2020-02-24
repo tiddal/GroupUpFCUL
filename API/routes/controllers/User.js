d587c7fcaf787cf35884ba812b34bdc1b29aee98
tiddal <tiddal@sapo.pt>
2020-02-24 15:54:03 +0000

    implement read from a json file to store users

--- API/routes/controllers/User.js
+++ API/routes/controllers/User.js
@@ -1,5 +1,24 @@
 const User = require('../../db/models/User');
 
+async function insertOne(req, res) {
+	const { name } = req.body;
+	try {
+		const user = await User.create({ name });
+		return res.json(user);
+	} catch (err) {
+		return res.status(400).json({ error: 'Bad Request' });
+	}
+}
+
+async function insertMany(req, res) {
+	try {
+		const users = await User.bulkCreate(req.body.users);
+		return res.json(users);
+	} catch (err) {
+		return res.status(400).json({ error: 'Bad Request' });
+	}
+}
+
 module.exports = {
 	async selectById(req, res) {
 		const user = await User.findByPk(req.params.id);
@@ -12,8 +31,12 @@ module.exports = {
 	},
 
 	async insert(req, res) {
-		const { name } = req.body;
-		const user = await User.create({ name });
-		return res.json(user);
+		const { option } = req.body;
+		if (option === 'insert_one') {
+			return insertOne(req, res);
+		} else if (option === 'insert_many') {
+			return insertMany(req, res);
+		}
+		return res.status(400).json({ error: 'Bad Request' });
 	}
 };
