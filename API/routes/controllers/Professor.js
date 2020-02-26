a79734b4fcc501db36399a815a3a3b1ec9098f90
tiddal <tiddal@sapo.pt>
2020-02-26 23:34:15 +0000

    getting professors now lists the respective user information

--- API/routes/controllers/Professor.js
+++ API/routes/controllers/Professor.js
@@ -3,13 +3,24 @@ const { status } = require('./utils');
 
 module.exports = {
 	selectAll: (req, res) => {
-		Professor.findAll()
+		Professor.findAll({
+			attributes: {
+				exclude: ['userId', 'createdAt', 'updatedAt']
+			},
+			include: { association: 'user' }
+		})
 			.then((professors) => res.json(professors))
 			.catch((err) => status(res, 500));
 	},
 
 	selectById: (req, res) => {
-		Professor.findOne({ where: { userId: req.params.id } })
+		Professor.findOne({
+			where: { userId: req.params.id },
+			attributes: {
+				exclude: ['userId', 'createdAt', 'updatedAt']
+			},
+			include: { association: 'user' }
+		})
 			.then((professor) => (professor ? res.json(professor) : status(res, 404)))
 			.catch((err) => status(res, 500));
 	}
--- API/routes/users.js
+++ API/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('./controllers/User');
 const StudentController = require('./controllers/Student');
-const ProfessorController = require('./controllers/Student');
+const ProfessorController = require('./controllers/Professor');
 const AdminsController = require('./controllers/Admin');
 
 router.get('/students/', StudentController.selectAll);
