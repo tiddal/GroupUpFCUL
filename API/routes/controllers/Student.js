95378afb80e7a5b9d26decc6e9a820de70cc178e
tiddal <tiddal@sapo.pt>
2020-02-26 23:18:46 +0000

    getting students now lists the respective user information

--- API/routes/controllers/Student.js
+++ API/routes/controllers/Student.js
@@ -3,13 +3,26 @@ const { status } = require('./utils');
 
 module.exports = {
 	selectAll: (req, res) => {
-		Student.findAll()
-			.then((students) => res.json(students))
+		Student.findAll({
+			attributes: {
+				exclude: ['userId', 'createdAt', 'updatedAt']
+			},
+			include: { association: 'user' }
+		})
+			.then((students) => {
+				res.json(students);
+			})
 			.catch((err) => status(res, 500));
 	},
 
 	selectById: (req, res) => {
-		Student.findOne({ where: { userId: req.params.id } })
+		Student.findOne({
+			where: { userId: req.params.id },
+			attributes: {
+				exclude: ['userId', 'createdAt', 'updatedAt']
+			},
+			include: { association: 'user' }
+		})
 			.then((student) => (student ? res.json(student) : status(res, 404)))
 			.catch((err) => status(res, 500));
 	}
