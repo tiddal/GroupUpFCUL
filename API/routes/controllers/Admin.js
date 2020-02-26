90393f30792579501b0dce1c0f9b4b2c116f790f
tiddal <tiddal@sapo.pt>
2020-02-26 23:37:44 +0000

    getting admins now lists the respective user information

--- API/routes/controllers/Admin.js
+++ API/routes/controllers/Admin.js
@@ -3,13 +3,24 @@ const { status } = require('./utils');
 
 module.exports = {
 	selectAll: (req, res) => {
-		Admin.findAll()
+		Admin.findAll({
+			attributes: {
+				exclude: ['userId', 'createdAt', 'updatedAt']
+			},
+			include: { association: 'user' }
+		})
 			.then((admins) => res.json(admins))
 			.catch((err) => status(res, 500));
 	},
 
 	selectById: (req, res) => {
-		Admin.findOne({ where: { userId: req.params.id } })
+		Admin.findOne({
+			where: { userId: req.params.id },
+			attributes: {
+				exclude: ['userId', 'createdAt', 'updatedAt']
+			},
+			include: { association: 'user' }
+		})
 			.then((admin) => (admin ? res.json(admin) : status(res, 404)))
 			.catch((err) => status(res, 500));
 	}
