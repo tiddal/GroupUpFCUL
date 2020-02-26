b71d2de6ec0adcf0fc589031f4257f1884a03d12
tiddal <tiddal@sapo.pt>
2020-02-26 18:22:21 +0000

    add two routes to list all students and to get a student by his user_id

---
+++ API/routes/controllers/Student.js
@@ -0,0 +1,16 @@
+const Student = require('../../db/models/Student');
+const { status } = require('./utils');
+
+module.exports = {
+	selectAll: (req, res) => {
+		Student.findAll()
+			.then((students) => res.json(students))
+			.catch((err) => status(res, 500));
+	},
+
+	selectById: (req, res) => {
+		Student.findAll({ where: { userId: req.params.id } })
+			.then((student) => (student ? res.json(student) : status(res, 404)))
+			.catch((err) => status(res, 500));
+	}
+};
--- API/routes/users.js
+++ API/routes/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('./controllers/User');
+const StudentController = require('./controllers/Student');
+
+router.get('/students/', StudentController.selectAll);
+router.get('/students/:id', StudentController.selectById);
 
 router.get('/', UserController.selectAll);
 router.get('/:id', UserController.selectById);
