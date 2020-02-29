f605fc56fdafd59995aed4e76e2deb03b8fe9f01
tiddal <tiddal@sapo.pt>
2020-02-29 00:16:59 +0000

    change the db configurations to support 3 different environments

--- API/db/config/config.js
+++ API/db/config/config.js
@@ -1,13 +1,37 @@
 require('dotenv').config();
 
 module.exports = {
-	dialect: 'mysql',
-	host: process.env.DB_HOST,
-	username: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	define: {
-		timestamps: true,
-		underscored: false
+	development: {
+		username: process.env.DEV_DB_USER,
+		password: process.env.DEV_DB_PASSWORD,
+		database: process.env.DEV_DB_NAME,
+		host: process.env.DEV_DB_HOST,
+		dialect: 'mysql',
+		define: {
+			timestamps: true,
+			underscored: false
+		}
+	},
+	test: {
+		username: process.env.TEST_DB_USER,
+		password: process.env.TEST_DB_PASSWORD,
+		database: process.env.TEST_DB_NAME,
+		host: process.env.TEST_DB_HOST,
+		dialect: 'mysql',
+		define: {
+			timestamps: true,
+			underscored: false
+		}
+	},
+	production: {
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		dialect: 'mysql',
+		define: {
+			timestamps: true,
+			underscored: false
+		}
 	}
 };
--- API/db/config/conn.js
+++ API/db/config/conn.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('./config');
+const env = process.env.NODE_ENV || 'development';
 
-const conn = new Sequelize(dbConfig);
+const conn = new Sequelize(dbConfig[env]);
 
 module.exports = conn;
