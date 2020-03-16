72b0e25523eecd821e51a800a4dc243ca5a3b2f9
tiddal <tiddal@sapo.pt>
2020-03-16 15:49:46 +0000

    fix error where the json on login was invalid

--- API/middleware/errorHandler.js
+++ API/middleware/errorHandler.js
@@ -1,4 +1,8 @@
-const errorHandler = (error, req, res, next) =>
-	res.status(error.status).json({ error });
+const errors = require('../utils/errors');
+
+const errorHandler = (error, req, res, next) => {
+	if (error.status === undefined) error = errors.DB_DOWN();
+	return res.status(error.status).json({ error });
+};
 
 module.exports = errorHandler;
--- API/routes/controllers/Auth.js
+++ API/routes/controllers/Auth.js
@@ -1,13 +1,22 @@
 const bcrypt = require('bcryptjs');
 const error = require('../../utils/errors');
+const assert = require('assert');
 
 const User = require('../../db/models/User');
 
 module.exports = {
 	login: (req, res, next) => {
+		try {
+			assert(req.body.user !== undefined);
+		} catch (err) {
+			return next(error.INVALID_JSON());
+		}
+
 		const { email, password } = req.body.user;
+
 		User.findOne({ where: { email } })
 			.then((user) => {
+				if (!user) return next(error.LOGIN_FAILED());
 				if (!user || !bcrypt.compareSync(password, user.password)) {
 					return next(error.LOGIN_FAILED());
 				}
