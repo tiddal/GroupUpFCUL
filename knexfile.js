82eb0331ba5f0e42b65e6e946e1ebb84d7f64912
tiddal <tiddal@sapo.pt>
2020-04-16 13:50:48 +0100

    refactor: migrate from sqlite to pgsql

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,46 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB,
-    },
-    migrations: {
-      directory: "./API/db/migrations",
-    },
-    seeds: {
-      directory: "./API/db/seeds",
-    },
-  },
+	development: {
+		client: 'pg',
+		connection: {
+			host: process.env.DB_HOST,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB,
+		},
+		migrations: {
+			directory: './API/db/migrations',
+		},
+		seeds: {
+			directory: './API/db/seeds',
+		},
+	},
 
-  test: {
-    client: "sqlite3",
-    connection: {
-      filename: "./API/db/db.test.sqlite",
-    },
-    migrations: {
-      directory: "./API/db/migrations",
-    },
-    seeds: {
-      directory: "./API/db/seeds",
-    },
-    useNullAsDefault: true,
-  },
+	test: {
+		client: 'pg',
+		connection: {
+			host: process.env.DB_HOST,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: 'groupup_test',
+		},
+		migrations: {
+			directory: './API/db/migrations',
+		},
+		seeds: {
+			directory: './API/db/seeds',
+		},
+	},
 
-  production: {
-    client: "pg",
-    connection: process.env.DB_URL,
-    migrations: {
-      directory: "./API/db/migrations",
-    },
-    seeds: {
-      directory: "./API/db/seeds",
-    },
-  },
+	production: {
+		client: 'pg',
+		connection: process.env.DB_URL,
+		migrations: {
+			directory: './API/db/migrations',
+		},
+		seeds: {
+			directory: './API/db/seeds',
+		},
+	},
 };
