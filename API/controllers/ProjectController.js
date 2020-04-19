5443bb2f7f8e71c675e0a05f5f375e1730ad09d2
fc51602 <fc51602@alunos.fc.ul.pt>
2020-04-19 14:49:44 +0100

    feat: project documentation and response and query changes

--- API/controllers/ProjectController.js
+++ API/controllers/ProjectController.js
@@ -1,202 +1,207 @@
-const connection = require('../db/config/connection');
-const errors = require('../utils/errors');
-const { v4: uuidv4 } = require('uuid');
+const connection = require("../db/config/connection");
+const errors = require("../utils/errors");
+const { v4: uuidv4 } = require("uuid");
 
 class ProjectController {
-	constructor() {
-		this.index = this.index.bind(this);
-		this.store = this.store.bind(this);
-		this.find = this.find.bind(this);
-		this.findByAcademicYear = this.findByAcademicYear.bind(this);
-		this.modify = this.modify.bind(this);
-		this.remove = this.remove.bind(this);
+  constructor() {
+    this.index = this.index.bind(this);
+    this.store = this.store.bind(this);
+    this.find = this.find.bind(this);
+    this.findByAcademicYear = this.findByAcademicYear.bind(this);
+    this.modify = this.modify.bind(this);
+    this.remove = this.remove.bind(this);
 
-		this.findCourse = this.findCourse.bind(this);
-		this.findUnit = this.findUnit.bind(this);
-	}
+    this.findCourse = this.findCourse.bind(this);
+    this.findUnit = this.findUnit.bind(this);
+  }
 
-	async index(request, response, next) {
-		const unit = await this.findUnit(request, response, next);
-		if (!unit) return next();
-		const projects = await connection('Project').select([
-			'name',
-			'min_students',
-			'max_students',
-			'description',
-			'objectives',
-			'assignment_url',
-			'academic_year',
-		]);
-		return response.json(projects);
-	}
+  async index(request, response, next) {
+    const unit = await this.findUnit(request, response, next);
+    if (!unit) return next();
+    const projects = await connection("Project")
+      .select([
+        "number",
+        "name",
+        "min_students",
+        "max_students",
+        "description",
+        "objectives",
+        "assignment_url",
+        "academic_year",
+      ])
+      .where("unit_id", unit.id);
+    return response.json(projects);
+  }
 
-	async store(request, response, next) {
-		const unit = await this.findUnit(request, response, next);
-		if (!unit) return next();
-		let { min_students, max_students } = request.body.project;
-		const {
-			name,
-			description,
-			objectives,
-			assignment_url,
-		} = request.body.project;
-		if (max_students < min_students) max_students = min_students;
-		let number = 1;
-		const unit_id = unit.id;
-		const id = uuidv4();
-		const date = new Date();
-		const academic_year =
-			date.getMonth() > 7
-				? `${date.getFullYear()}-${date.getFullYear() + 1}`
-				: `${date.getFullYear() - 1}-${date.getFullYear()}`;
-		const [existentProject] = await connection('Project')
-			.select('number')
-			.where({ academic_year, unit_id })
-			.orderBy('number', 'desc')
-			.limit(1);
-		if (existentProject) number = existentProject.number + 1;
-		try {
-			const [project] = await connection('Project').insert(
-				{
-					id,
-					number,
-					unit_id,
-					name,
-					academic_year,
-					min_students,
-					max_students,
-					description,
-					objectives,
-					assignment_url,
-				},
-				['number', 'name', 'academic_year']
-			);
-			return response.status(201).json(project);
-		} catch (error) {
-			return next(errors.UNIQUE_CONSTRAIN(error.detail));
-		}
-	}
+  async store(request, response, next) {
+    const unit = await this.findUnit(request, response, next);
+    if (!unit) return next();
+    let { min_students, max_students } = request.body.project;
+    const {
+      name,
+      description,
+      objectives,
+      assignment_url,
+    } = request.body.project;
+    if (max_students < min_students) max_students = min_students;
+    let number = 1;
+    const unit_id = unit.id;
+    const id = uuidv4();
+    const date = new Date();
+    const academic_year =
+      date.getMonth() > 7
+        ? `${date.getFullYear()}-${date.getFullYear() + 1}`
+        : `${date.getFullYear() - 1}-${date.getFullYear()}`;
+    const [existentProject] = await connection("Project")
+      .select("number")
+      .where({ academic_year, unit_id })
+      .orderBy("number", "desc")
+      .limit(1);
+    if (existentProject) number = existentProject.number + 1;
+    try {
+      const [project] = await connection("Project").insert(
+        {
+          id,
+          number,
+          unit_id,
+          name,
+          academic_year,
+          min_students,
+          max_students,
+          description,
+          objectives,
+          assignment_url,
+        },
+        ["number", "name", "academic_year"]
+      );
+      return response.status(201).json(project);
+    } catch (error) {
+      return next(errors.UNIQUE_CONSTRAIN(error.detail));
+    }
+  }
 
-	async find(request, response, next) {
-		const unit = await this.findUnit(request, response, next);
-		if (!unit) return next();
-		const {
-			project_year: academic_year,
-			project_number: number,
-		} = request.params;
-		const [project] = await connection('Project')
-			.select([
-				'name',
-				'min_students',
-				'max_students',
-				'description',
-				'objectives',
-				'assignment_url',
-				'academic_year',
-			])
-			.where({ academic_year, unit_id: unit.id, number });
-		if (!project)
-			return errors.PROJECT_NOT_FOUND(academic_year, number, 'params');
-		return response.json(project);
-	}
+  async find(request, response, next) {
+    const unit = await this.findUnit(request, response, next);
+    if (!unit) return next();
+    const {
+      project_year: academic_year,
+      project_number: number,
+    } = request.params;
+    const [project] = await connection("Project")
+      .select([
+        "number",
+        "name",
+        "min_students",
+        "max_students",
+        "description",
+        "objectives",
+        "assignment_url",
+        "academic_year",
+      ])
+      .where({ academic_year, unit_id: unit.id, number });
+    if (!project)
+      return errors.PROJECT_NOT_FOUND(academic_year, number, "params");
+    return response.json(project);
+  }
 
-	async findByAcademicYear(request, response, next) {
-		const unit = await this.findUnit(request, response, next);
-		if (!unit) return next();
-		const { project_year: academic_year } = request.params;
-		const projects = await connection('Project')
-			.select([
-				'name',
-				'min_students',
-				'max_students',
-				'description',
-				'objectives',
-				'assignment_url',
-				'academic_year',
-			])
-			.where({ academic_year, unit_id: unit.id });
-		return response.json(projects);
-	}
+  async findByAcademicYear(request, response, next) {
+    const unit = await this.findUnit(request, response, next);
+    if (!unit) return next();
+    const { project_year: academic_year } = request.params;
+    const projects = await connection("Project")
+      .select([
+        "name",
+        "min_students",
+        "max_students",
+        "description",
+        "objectives",
+        "assignment_url",
+        "academic_year",
+      ])
+      .where({ academic_year, unit_id: unit.id });
+    return response.json(projects);
+  }
 
-	async modify(request, response, next) {
-		const project = await this.findProject(request, response, next);
-		if (!project) return next();
-		let { min_students, max_students } = request.body.project;
-		if (max_students < min_students) max_students = min_students;
-		const {
-			name,
-			description,
-			objectives,
-			assignment_url,
-		} = request.body.project;
-		const [updatedProject] = await connection('Project').where(project).update(
-			{
-				name,
-				min_students,
-				max_students,
-				description,
-				objectives,
-				assignment_url,
-			},
-			[
-				'name',
-				'min_students',
-				'max_students',
-				'description',
-				'objectives',
-				'assignment_url',
-				'academic_year',
-			]
-		);
+  async modify(request, response, next) {
+    const project = await this.findProject(request, response, next);
+    if (!project) return next();
+    let { min_students, max_students } = request.body.project;
+    if (max_students < min_students) max_students = min_students;
+    const {
+      name,
+      description,
+      objectives,
+      assignment_url,
+    } = request.body.project;
+    const [updatedProject] = await connection("Project").where(project).update(
+      {
+        name,
+        min_students,
+        max_students,
+        description,
+        objectives,
+        assignment_url,
+      },
+      [
+        "number",
+        "name",
+        "min_students",
+        "max_students",
+        "description",
+        "objectives",
+        "assignment_url",
+        "academic_year",
+      ]
+    );
 
-		return response.json(updatedProject);
-	}
+    return response.json(updatedProject);
+  }
 
-	async remove(request, response, next) {
-		const project = await this.findProject(request, response, next);
-		if (!project) return next();
-		await connection('Project').where(project).del();
-		return response.status(204).send();
-	}
+  async remove(request, response, next) {
+    const project = await this.findProject(request, response, next);
+    if (!project) return next();
+    await connection("Project").where(project).del();
+    return response.status(204).send();
+  }
 
-	async findCourse(request, response, next) {
-		const { code } = request.params;
-		const [course] = await connection('Course').select('id').where({ code });
-		if (!course) return next(errors.COURSE_NOT_FOUND(code, 'params'));
-		return course;
-	}
+  async findCourse(request, response, next) {
+    const { code } = request.params;
+    const [course] = await connection("Course").select("id").where({ code });
+    if (!course) return next(errors.COURSE_NOT_FOUND(code, "params"));
+    return course;
+  }
 
-	async findUnit(request, response, next) {
-		const course = await this.findCourse(request, response, next);
-		if (!course) return next();
-		const { unit_code } = request.params;
-		const [unit] = await connection('course_unit')
-			.join('Unit', 'Unit.id', '=', 'course_unit.unit_id')
-			.select('Unit.id')
-			.where({
-				'course_unit.course_id': course.id,
-				'Unit.code': unit_code,
-			});
-		if (!unit) return next(errors.UNIT_NOT_FOUND(unit_code, 'params'));
-		return unit;
-	}
+  async findUnit(request, response, next) {
+    const course = await this.findCourse(request, response, next);
+    if (!course) return next();
+    const { unit_code } = request.params;
+    const [unit] = await connection("course_unit")
+      .join("Unit", "Unit.id", "=", "course_unit.unit_id")
+      .select("Unit.id")
+      .where({
+        "course_unit.course_id": course.id,
+        "Unit.code": unit_code,
+      });
+    if (!unit) return next(errors.UNIT_NOT_FOUND(unit_code, "params"));
+    return unit;
+  }
 
-	async findProject(request, response, next) {
-		const unit = await this.findUnit(request, response, next);
-		if (!unit) return next();
-		const {
-			project_year: academic_year,
-			project_number: number,
-		} = request.params;
-		const [project] = await connection('Project').select('id').where({
-			unit_id: unit.id,
-			academic_year,
-			number,
-		});
-		if (!project)
-			return errors.PROJECT_NOT_FOUND(academic_year, number, 'params');
-		return project;
-	}
+  async findProject(request, response, next) {
+    const unit = await this.findUnit(request, response, next);
+    if (!unit) return next();
+    const {
+      project_year: academic_year,
+      project_number: number,
+    } = request.params;
+    const [project] = await connection("Project").select("id").where({
+      unit_id: unit.id,
+      academic_year,
+      number,
+    });
+    if (!project)
+      return errors.PROJECT_NOT_FOUND(academic_year, number, "params");
+    return project;
+  }
 }
 
 module.exports = new ProjectController();
